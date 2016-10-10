9061331368da3702f4429229b973a68034979def
Evgeniy <evgver@bk.ru>
2016-10-10 16:11:51 +0300

    Some buttons target. Not finished
    Left menu setup changed a bit.

--- iAmReporter_V3/iAmReporter_V3/LeftMenuCell.swift
+++ iAmReporter_V3/iAmReporter_V3/LeftMenuCell.swift
@@ -12,7 +12,29 @@ class LeftMenuCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-            setupView()
+        setupView()
+        
+        allUkraineButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        addNewsButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        myReportersButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        answersButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        readersButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        topNewsButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        allReportersButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        allNewsButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        politicButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        lifeButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        humorButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        sportButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        otherButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        favoriteButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        rateAppButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        sendMailButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+    }
+    
+    func buttonTapped(sender: UIButton) {
+        print(sender.buttonType)
     }
     
     let topBarView: UIView = {
@@ -98,18 +120,18 @@ class LeftMenuCell: UICollectionViewCell {
         addConstraintsWithFormat("H:|[v0]|", views: separator6)
         
         addConstraintsWithFormat("V:|[v0(105)]"
-            + "-14-[v1(31)]"
-            + "-14-[v17(1)]"
-            + "-14-[v2(31)]"
-            + "-14-[v18(1)]"
-            + "-14-[v3(31)][v4(31)][v5(31)]"
-            + "-14-[v19(1)]"
-            + "-14-[v6(31)][v7(31)]"
-            + "-14-[v20(1)]"
-            + "-14-[v8(31)][v9(31)][v10(31)][v11(31)][v12(31)][v13(31)][v14(31)]"
-            + "-14-[v21(1)]"
-            + "-14-[v15(31)][v16(31)]"
-            + "-14-[v22(1)]",
+            + "-6-[v1(46)]"
+            + "-6-[v17(1)]" // separator
+            + "-6-[v2(46)]"
+            + "-6-[v18(1)]" // separator
+            + "-6-[v3(36)][v4(36)][v5(36)]"
+            + "-6-[v19(1)]" // separator
+            + "-6-[v6(36)][v7(36)]"
+            + "-6-[v20(1)]" // separator
+            + "-6-[v8(36)][v9(36)][v10(36)][v11(36)][v12(36)][v13(36)][v14(36)]"
+            + "-6-[v21(1)]" // separator
+            + "-6-[v15(36)][v16(36)]"
+            + "-6-[v22(1)]", // separator
             views: topBarView,
             allUkraineButton,
             addNewsButton,
@@ -128,16 +150,28 @@ class LeftMenuCell: UICollectionViewCell {
 func createButton (name: String, logo: String) -> UIButton {
     
     let attachment = NSTextAttachment()
-    attachment.image = UIImage(named: logo)
+    attachment.image = UIImage(named: logo)?.withRenderingMode(.alwaysOriginal)
     attachment.bounds = CGRect.init(x: 0, y: -3, width: 19, height: 19)
     let attributedText = NSMutableAttributedString()
     attributedText.append(NSAttributedString(attachment: attachment))
     attributedText.append(NSAttributedString(string: "   \(name)", attributes: [NSForegroundColorAttributeName: UIColor.black]))
     
+    let attachmentTap = NSTextAttachment()
+    attachmentTap.image = UIImage(named: logo)?.withRenderingMode(.alwaysTemplate)
+    attachmentTap.bounds = CGRect.init(x: 0, y: -3, width: 19, height: 19)
+    let attributedTextTap = NSMutableAttributedString()
+    attributedTextTap.append(NSAttributedString(attachment: attachmentTap))
+    attributedTextTap.append(NSAttributedString(string: "   \(name)", attributes: [NSForegroundColorAttributeName: UIColor.black]))
+    
     let button = UIButton()
     button.setAttributedTitle(attributedText, for: .normal)
+    
+    button.setAttributedTitle(attributedTextTap, for: .highlighted)
+    button.tintColor = UIColor.red
+    
     button.titleLabel?.font = UIFont(name: "SFUIDisplay Regular", size: 18)
     button.contentHorizontalAlignment = .left
+    
     
     return button
 }
